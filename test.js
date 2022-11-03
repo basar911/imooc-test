c62b8a721ee4402148b2c63778fe01641664165f
文刀走云 <523094696@qq.com>
2022-11-03 13:58:25 +0800

    修改dev-test.js

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-window.onload = function () {
-  let test = document.getElementsByClassName("test")[0];
-  test.addEventListener("click", () => {
-    alert("this is test code");
-  });
-};
+console.log("test.js");
+console.log("this is branch of dev");
